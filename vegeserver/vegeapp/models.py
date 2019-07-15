aa3abe1ba84ef964b722bac99338c8d856d27b6e
Alex <32681448+dontknow9179@users.noreply.github.com>
2019-07-15 14:24:02 +0800

    models test

--- vegeserver/vegeapp/models.py
+++ vegeserver/vegeapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+import mongoengine
+import datetime
+from mongoengine.fields import *
+import vegeserver.config as config
+
+
+class Article(Document):
+    # Meta variables.
+    meta = {
+        'collection': config.MONGODB_ARTICLE_COLLECTION
+    }
+
+    # Document variables.
+    id = StringField(primary_key=True)
+    time = StringField()
+    types = ListField(StringField(), default=list)
+    tags = ListField(StringField(), default=list)
+    source = StringField()
+    channel = StringField()
+    title = StringField()
+    url = StringField()
+    code = StringField()
+    content = StringField()
+    imgUrls = ListField(StringField(), default=list)
+
