49d36e140e38dfe8fdcabee3915e2e2aa7af6174
dontknow9179 <16307130343@fudan.edu.cn>
2019-07-15 16:53:04 +0800

    test again

--- vegeserver/vegeserver/urls.py
+++ vegeserver/vegeserver/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from .import view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('/', view.hello),
+    path('', view.hello),
+    path('admin/', admin.site.urls),   
 ]
