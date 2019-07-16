5cb7e18c9dc6752c682a5d4610510e86084165f1
dontknow9179 <16307130343@fudan.edu.cn>
2019-07-16 13:02:10 +0800

    迷

--- vegeserver/vegeserver/view.py
+++ vegeserver/vegeserver/view.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from vegeapp.models import Article
 
-def homepage(self, request):
+def homepage(request):
     result = Article.objects.get(id="articleID-d7971f82-38f0-4060-b065-5149a5413c8c")
     result = result.tags
     return HttpResponse(result)
