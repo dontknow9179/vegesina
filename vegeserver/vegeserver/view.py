6ba0ce3082ddbfc22d9a1b2011e33f6e5a0e440f
dontknow9179 <16307130343@fudan.edu.cn>
2019-07-16 15:41:53 +0800

    index blank

--- vegeserver/vegeserver/view.py
+++ vegeserver/vegeserver/view.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from vegeapp.models import Article
 
 def homepage(request):
-    result = Article.objects.get(id="articleID-d7971f82-38f0-4060-b065-5149a5413c8c")
-    result = result.tags
-    return HttpResponse(result)
+    # result = Article.objects.get(id="articleID-d7971f82-38f0-4060-b065-5149a5413c8c")
+    # result = result.tags
+    # return HttpResponse(result)
+    return render(request, "index.html")
